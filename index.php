8b7e600a25171b44f659b1763f55d73d85e37ed4
AlirezaMolyanian <127932630+AlirezaMolyanian@users.noreply.github.com>
2023-03-15 14:09:05 +0330

    Add files via upload

---
+++ index.php
@@ -0,0 +1,163 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lachinani Plants & Flower</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="icon" href="./images/logo.jpg">
+    <meta name="keywords" content="گل فروشی,فروش گل های زینتی">
+</head>
+
+<body>
+    <div class="header">
+        <img src="images/header-img1.jpg" alt="">
+
+        <div class="header-menu">
+            <a href="#" class="top-item" title="لیست کاربری">
+                <img src="images/icons8-menu-64.png" alt="">
+            </a>
+            <a href="#" title="حساب کاربری">
+                <img src="images/User.png" alt="">
+            </a>
+            <a href="#" title="مشاهده ی سبد خرید">
+                <img src="images/icons8-add-shopping-cart-100.png" alt="">
+            </a>
+            <a href="#allOP" title="مشاهده ی محصولات">
+                <img src="images/icons8-yucca-64.png" alt="">
+            </a>
+        </div>
+    </div>
+    <a href="#connect-info-link" class="phone" title="تماس با ما">
+        <img src="images/phone.png" alt="" class="phone-img">
+    </a>
+    <div class="big-holder">
+        <div class="bigholer__menuholder">
+            <div class="menuholder__left">
+            </div>
+            <div class="menuholder__main">
+                <a href="./images/گیاه گوشتخوار کوزه ای(نپنتیس).jpg" class="options">
+                    <img src="images/گیاه گوشتخوار کوزه ای(نپنتیس).jpg" alt="گیاه گوشتخوار کوزه ای">
+                    <h4 class="frontp">کوزه‌ای (نپنتیس)</h4>
+                    <p class="backp">
+                        این گونه از گیاهان گوشتخوار در انتهای خود ساختار کوزه مانندی دارن که داخلش نوعی
+                        مایع حاوی آنزیم وجود داره و زمانی که حشره ها به دام میوفتن ، گیاه اونا رو هضم و مواد
+                        .مغذیشونو جذب میکنه
+                    </p>
+                </a>
+                <a href="./images/گیاه گوتشخوار ونوس.jpg" class="options">
+                    <img src="images/گیاه گوتشخوار ونوس.jpg" alt="گیاه گوشتخوار ونوس">
+                    <h4 class="frontp">ونوس (Venus fly trap)</h4>
+                    <p class="backp">
+                        .با اختلاف میشه گفت شگفت انگیز ترین گیاهی هست که توی قلمرو گیاهان پیدا میشه
+                        <br>
+                        .زمانی که حشره وارد تله ونوس میشه ، تله با سرعت زیادی بسته میشه و حشره رو شکار میکنه
+                    </p>
+                </a>
+                <a href="./images/گیاه گوشتخوار پینگویکولا.jpg" class="options">
+                    <img src="images/گیاه گوشتخوار پینگویکولا.jpg" alt="گیاه گوشتخوار پینگویکولا">
+                    <h4 class="frontp">پینگویکولا (Pinguicula)</h4>
+                    <p class="backp">
+                        .جزو اون گیاه هایی هست که اصلا بهش نمیاد گوشتخوار باشه ، ولی اینجاس که نباید گول ظاهر رو خورد
+                        <br>
+                        .روی برگ های گیاه پرز های ریز و چسبناکی وجود داره که با اونا حشرات ریز رو شکار میکنه
+                    </p>
+                </a>
+                <a href="./images/گیاه گوشتخوار سفالتوس.jpg" class="options">
+                    <img src="images/گیاه گوشتخوار سفالتوس.jpg" alt="گیاه گوشتخوار سفالیتوس">
+                    <h4 class="frontp">سفالیتوسی(Cephalotus)</h4>
+                    <p class="backp">
+                        این گونه بومی استرالیا هست و جزو گونه های کمیاب و در حال انقراض گیاه های گوشتخوار
+                        .محسوب میشه
+                        <br>
+                        برگ های کوزه مانندی داره و اختصاصا برای شکار مورچه ها طراحی شده ولی باقی حشرات رو هم گاها شکار
+                        .میکنه
+                    </p>
+                </a>
+                <a href="./images/گیاه گوشتخوار ساراسنیا.jpg" class="options">
+                    <img src="images/گیاه گوشتخوار ساراسنیا.jpg" alt="گیاه گوتشخوار ساراسنیا">
+                    <h4 class="frontp">ساراسنیا(Sarracenia)</h4>
+                    <p class="backp">
+                        ساراسنیا جزو بزرگ ترین گونه های گیاه گوشتخواره که بعضی نژاد های اون در سایز بزرگ توانایی شکار
+                        .پرنده های کوچک رو هم دارن
+                        .برگ های گیاه حالت پارچ مانندی داره و داخلش با مایه حاوی آنزیم پر شده
+                    </p>
+                </a>
+            </div>
+            <div class="menuholder__right">
+            </div>
+        </div>
+        <div class="showback">
+            <a href="" class="showback__btn"> ثبت سفارش</a>
+            <a href="" class="showback__btn">تماس با ما</a>
+        </div>
+    </div>
+
+    <div class="big-holder1">
+        <div class="midel_menu">
+            <div class="for-img">
+                <img src="images/mohammad.jpg" alt="">
+            </div>
+            <hr>
+            <div class="text-holder">
+                <h3>مطالب آموزشی</h3>
+                <?php
+                    require "information.php";
+                ?>
+            </div>
+            <hr style="width: 100%;" id="allOP">
+            <h2>لیست محصولات</h2>
+            <div class="all-options">
+                <a href="" class="new-options">
+                    <img src="images/eatingFlower.jpg" alt="">
+                    <p>گیاهان گوشت خوار</p>
+                </a>
+                <a href="" class="new-options">
+                    <img src="images/hflower.jpg" alt="">
+                    <p>گیاهان هوازی</p>
+                </a>
+                <a href="" class="new-options">
+                    <img src="images/cactoos.jpg" alt="">
+                    <p>کاکتوس و گیاهان زینتی</p>
+                </a>
+            </div>
+            <a href="">مشاهده ی همه ی محصولات</a>
+        </div>
+        <div class="showback">
+        </div>
+        <div class="footer">
+            <div class="address-info">
+                <h3>آدرس : </h3>
+                <p>
+                    استان اصفهان - شهرستان فریدونشهر - خیابان مدنی - خیابان کوهساران - بنبست شقایق
+                </p>
+                <h3>
+                    شماره ی تماس: 09909090
+                </h3>
+            </div>
+            <div class="connect-info">
+                <div id="connect-info-link">
+                    <a href="https://instagram.com/lachinashvili_mohamad?igshid=YmMyMTA2M2Y=" class="info-item">
+                        <img src="images/icons8-instagram-100.png" alt="">
+                        <p>Instagram : <span style="font-style: italic;">راه ارتباطی(کلیک کنید)</span></p>
+                    </a>
+                    <a href="tg://resolve?domain=@MlachinaniM" class="info-item">
+                        <img src="images/icons8-telegram-app-48.png" alt="">
+                        <p>Telegram : <span style="font-style: italic;">راه ارتباطی(کلیک کنید)</span></p>
+                    </a>
+                    <a href="#" class="info-item">
+                        <img src="images/icons8-gmail-100.png" alt="">
+                        <p>G-mail : <span style="font-style: italic;">راه ارتباطی(کلیک کنید)</span></p>
+                    </a>
+                    <a href="https://wa.me/+989306914729" class="info-item">
+                        <img src="images/icons8-whatsapp-64.png" alt="">
+                        <p>Whatsapp : <span style="font-style: italic;">راه ارتباطی(کلیک کنید)</span></p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
